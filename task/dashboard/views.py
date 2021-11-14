beb5ed5b4e9d9b78472fd1f6661d7ff278069828
fahiz <fahizkppullara@gmail.com>
2021-11-14 16:50:25 +0530

    templates

--- task/dashboard/views.py
+++ task/dashboard/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from order.models import Order, OrderStatus
+
+
+
+def quotesview(request):
+    
+    queryset = Order.objects.all()
+    x=[1,2,3,4]
+    context = {
+        'queryset': queryset,
+        'x': x
+    }
+    for i in queryset:
+        print(i.data_order_placed)
+        for j in i.order_item.all():
+            print(j.order_item_quantity)
+    return render(request, 'dashboard.html',context=context)
+    
